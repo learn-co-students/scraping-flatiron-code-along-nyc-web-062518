ad62b23ee4c88dd99c99e02e545e1bfcd16670b7
yunglleung1 <yungl.leung1@gmail.com>
2018-07-02 16:48:58 -0400

    Done.

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,19 @@
+# used to create course objects based on scraped data
 
+class Course
+  @@all = []
+  attr_accessor :title, :schedule, :description
 
+  def initialize
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+end
